2dd43463d1d5b062c87e22190bbf00adc9d1b3b9
Chenggggg <J_C_GUAN@163.com>
2016-08-02 08:49:21 +0800

    Initial commit

---
+++ app/src/main/java/com/samoy/asyncload/NewsBean.java
@@ -0,0 +1,33 @@
+package com.samoy.asyncload;
+
+
+public class NewsBean
+{
+    private String newsIconUrl;
+    private String newsTitle;
+    private String newsContent;
+
+    public String getNewsContent() {
+        return newsContent;
+    }
+
+    public void setNewsContent(String newsContent) {
+        this.newsContent = newsContent;
+    }
+
+    public String getNewsIconUrl() {
+        return newsIconUrl;
+    }
+
+    public void setNewsIconUrl(String newsIconUrl) {
+        this.newsIconUrl = newsIconUrl;
+    }
+
+    public String getNewsTitle() {
+        return newsTitle;
+    }
+
+    public void setNewsTitle(String newsTitle) {
+        this.newsTitle = newsTitle;
+    }
+}
